76467027af0d9ffb2f0dc6c2c75fe9ca81b16948
dushanir <dusha.uom@gmail.com>
2022-02-03 09:11:33 +0530

    meaningful labels

--- mapper.py
+++ mapper.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import sys
-row_a = 2
-col_b = 2 
+r_m = 2
+c_n = 2 
+line_index=0
 
 for line in sys.stdin:
-	matrix_index=0
     row_values = map(int,line.split())
-	if matrix_index < row_a:
+	if line_index < r_m:
         for j in range(0, len(row_values)):
-			for k in range(0, col_b): 
-                key = str(matrix_index) + "," + str(k)
+			for k in range(0, c_n): 
+                key = str(line_index) + "," + str(k)
                 print "%s\t%s\t%s"%(key,j,row_values[j])
 	else:
         for j in range(0, len(row_values)):
-			for k in range(0, row_a): 
+			for k in range(0, r_m): 
                 key =  str(k)  + "," + str(j)
-                print "%s\t%s\t%s"%(key,str(matrix_index-row_a),row_values[j])
-    matrix_index = matrix_index + 1
+                print "%s\t%s\t%s"%(key,str(line_index-r_m),row_values[j])
+    line_index = line_index + 1
 
